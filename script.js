b0b32c0c9e1c4d780d836a27474b770eeb8ca106
guiguitator <53449177+guiguitator@users.noreply.github.com>
2021-01-09 11:20:37 +0100

    Correction d'un bug concernant le Dark Mode

--- script.js
+++ script.js
@@ -1,33 +1,39 @@
 var core = document.getElementById("core");
 var aside = document.getElementById("aside");
-var card = document.getElementById("card");
 var search = document.getElementById("search");
 var link = document.getElementById("link");
 var theme_div = document.getElementById("theme_div");
 var theme = document.getElementById("theme");
+var card = document.querySelectorAll(".card");
+
+var i;
 
 function changeThemeToDark() {
 	core.style.backgroundColor = "#0a0a0a";
 	aside.style.backgroundColor = "#161616";
 	aside.style.color = "white";
-	card.style.backgroundColor = "#1c1c1c";
-	card.style.color = "white";
 	search.style.backgroundColor = "#2f3030";
 	search.style.color = "white";
 	link.style.color = "white";
 	theme_div.style.backgroundColor = "#161616";
 	theme.setAttribute('onclick',"changeThemeToLight()");
+		for (i = 0; i < card.length; i++) {
+			card[i].style.backgroundColor = "#1c1c1c";
+			card[i].style.color = "white";
+		}
 }
 
 function changeThemeToLight() {
 	core.style.backgroundColor = "#f7f7f7";
 	aside.style.backgroundColor = "white";
 	aside.style.color = "black";
-	card.style.backgroundColor = "white";
-	card.style.color = "black";
 	search.style.backgroundColor = "white";
 	search.style.color = "black";
 	link.style.color = "black";
 	theme_div.style.backgroundColor = "white";
 	theme.setAttribute('onclick',"changeThemeToDark()");
+		for (i = 0; i < card.length; i++) {
+			card[i].style.backgroundColor = "white";
+			card[i].style.color = "black";
+		}
 }
